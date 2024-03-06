2fbb11228a1005280babad5ab130617d185ebd28
Shrutam Agravat <Sagravat0518@conestogac.on.ca>
2024-03-06 13:58:52 -0500

    Test case completed for Isosceles Triangle

---
+++ TriangleValidator.cs
@@ -0,0 +1,21 @@
+﻿
+namespace TriangleSolver
+{
+    public class TriangleValidator
+    {
+        public string ValidateTriangle(int sideOne, int sideTwo, int sideThree)
+        {
+            //rule for equilateral triangle
+            if (sideOne == sideTwo && sideTwo == sideThree)
+            {
+                return "Equilateral Triangle";
+            }
+            //rule for isosceles  triangles
+            if (sideOne == sideTwo || sideOne == sideThree || sideTwo == sideThree)
+            {
+                return "Isosceles Triangle";
+            }
+            return "Invalid Response";
+        }
+    }
+}
--- TriangleValidatorTests.cs
+++ TriangleValidatorTests.cs
@@ -20,5 +20,44 @@ namespace TriangleSolverSQ
             // Assert
             Assert.AreEqual("Equilateral Triangle", result);
         }
+
+        // Validate Isosceles Triangle
+        [TestMethod]
+        public void ValidateIsoscelesTriangleFirst()
+        {
+            // Arrange
+            TriangleValidator validator = new TriangleValidator();
+
+            // Act
+            string result = validator.ValidateTriangle(9, 9, 1);
+
+            // Assert
+            Assert.AreEqual("Isosceles Triangle", result);
+        }
+        [TestMethod]
+        public void ValidateIsoscelesTriangleSecond()
+        {
+            // Arrange
+            TriangleValidator validator = new TriangleValidator();
+
+            // Act
+            string result = validator.ValidateTriangle(9, 1, 9);
+
+            // Assert
+            Assert.AreEqual("Isosceles Triangle", result);
+        }
+
+        [TestMethod]
+        public void ValidateIsoscelesTriangleThird()
+        {
+            // Arrange
+            TriangleValidator validator = new TriangleValidator();
+
+            // Act
+            string result = validator.ValidateTriangle(1, 9, 9);
+
+            // Assert
+            Assert.AreEqual("Isosceles Triangle", result);
+        }
     }
 }

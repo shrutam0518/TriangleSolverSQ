e1c4cb329a617f7b5f668da94f792674d57b31db
Shrutam Agravat <Sagravat0518@conestogac.on.ca>
2024-03-06 13:52:40 -0500

    Test case for Equilateral Triangle

--- TriangleValidatorTests.cs
+++ TriangleValidatorTests.cs
@@ -1,7 +1,24 @@
 ﻿
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TriangleSolver;
+
 namespace TriangleSolverSQ
 {
+    [TestClass]
     public class TriangleValidatorTests
     {
+        // Validate Equilateral Triangle
+        [TestMethod]
+        public void ValidateEquilateralTriangle()
+        {
+            // Arrange
+            TriangleValidator validator = new TriangleValidator();
+
+            // Act
+            string result = validator.ValidateTriangle(9, 9, 9);
+
+            // Assert
+            Assert.AreEqual("Equilateral Triangle", result);
+        }
     }
 }
